783fe3da7f23ece8d8963b9d6c34aacbc00eea83
nguoiay2001 <anhdung5c1@gmail.com>
2023-04-01 00:06:14 +0700

    Sửa login

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,28 +1,62 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import images from '~/assets/images';
+import Tippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import { Wrapper as PoperWrapper } from '~/components/Poper';
+import AccountItem from '~/components/SearchAccountItem/AccountItem';
+
+import Button from '~/components/Button';
 
 const cx = classNames.bind(styles);
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([]);
+        }, 2000);
+    }, []);
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="Tiktok" />
                 </div>
-                <div className={cx('search')}>
-                    <input placeholder="Search account and video" />
-                    <button className={cx('clear-btn')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
+                <Tippy
+                    interactive={true}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PoperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PoperWrapper>
+                        </div>
+                    )}
+                    visible={searchResult.length > 0}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search account and video" />
+                        <button className={cx('clear-btn')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('actions')}>
+                    <Button text>Login</Button>
+                    <Button rounded>Login</Button>
                 </div>
-                <div className={cx('actions')}>{/* Action */}</div>
             </div>
         </header>
     );
---
+++ src/components/SearchAccountItem/AccountItem/index.js
@@ -0,0 +1,26 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                src="https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/325608105_1148845239116999_3045381348987310331_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=UXsDwjfpL2QAX8Hyw6C&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfDd_1E5q8Ap3e3uYpcGczSwrdDaQO34VyjGZbmWD6zVNQ&oe=642936B6"
+                alt="Ảnh đại diện"
+                className={cx('avatar')}
+            />
+            <div className={cx('infor')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon icon={faCheckCircle} className={cx('check')} />
+                </h4>
+                <span className={cx('username')}>anhdung5c1</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
